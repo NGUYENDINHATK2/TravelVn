460002930a98840912a682a40fa36a3929d64500
nguyendinhatvku <81765926+nguyendinhatvku@users.noreply.github.com>
2021-10-23 22:16:18 +0700

    commit đâu tien

---
+++ resources/views/index.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<section  class="travel-box">
+
+
+
+	<header class="header-index">
+		
+		<div class="search-form">
+
+			
+				<div class="tab bg-info menu-search" >
+					<div class="row p-0">
+						<div class="col-sm-4 col-md-4 col-lg-4 p-0 boder" >
+							<button class="tablinks fs-3 " onclick="openCity(event, 'tour')" id="defaultOpen" ata-bs-toggle="tooltip" data-bs-placement="top" title="Tìm Kiếm Tour Du Lịch"><b>Tour</b></button>
+						</div>
+						<div class="col-sm-4 col-md-4 col-lg-4 p-0">
+							<button class="tablinks fs-3" onclick="openCity(event, 'hotel')" id="defaultOpen" ata-bs-toggle="tooltip" data-bs-placement="left" title="Tìm Kiếm Khách Sạn" ><b>Hotel</b></button>
+						</div>
+						<div class="col-sm-4 col-md-4 col-lg-4 p-0">
+							<button class="tablinks fs-3" onclick="openCity(event, 'info')" id="defaultOpen" ata-bs-toggle="tooltip" data-bs-placement="left" title="Tìm Thông Tin Địa Điểm"><b>Thông Tin Du Lịch</b></button>
+						</div>
+			
+					</div>
+					
+			
+				  </div>
+				  
+				  <div id="tour" class="tabcontent menu-search-tour pe-3 ps-3 ">
+					<div class="container-fuild p-2 pt-5">
+						
+						<form action="">
+							<h2 class="pb-2"><b>Địa Điểm Du Lịch</b></h2>
+							<div class="row border input-search-tourmain me-3 ms-3   ">
+
+							
+								
+									<div class="col-sm-1 col-md-1 col-lg-1 text-center p-2"> 
+										<i class="fas fa-map-marker-alt fa-3x text-info"></i>
+									</div>
+										<div  class=" col-sm-11 col-md-11 col-lg-11  input-search-tour">
+											
+											<input type="text" class="form-control border-bottom" id="exampleInputEmail1" aria-describedby="emailHelp">
+											
+										</div>
+							
+  
+							</div>
+							<div class="row">
+								a
+								
+							</div>
+							<div class="row">
+								<button class="btn btn-primary" type="submit"> Tìm Kiếm</button>
+								
+							</div>
+
+						</form>
+
+					</div>
+				  </div>
+				  
+				  <div id="hotel" class="tabcontent">
+					<h3>Paris</h3>
+					<p>Paris is the capital of France.</p> 
+				  </div>
+				  <div id="info" class="tabcontent">
+					<h3>Parisqqqqqqq</h3>
+					<p>Paris is the capital of France.</p> 
+				  </div>
+			
+			   
+			   </div>
+
+	
+
+	</header>
+
+	<main>
+
+	</main>
+		
+	<script>
+        function openCity(evt, cityName) {
+          var i, tabcontent, tablinks;
+          tabcontent = document.getElementsByClassName("tabcontent");
+          for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+          }
+          tablinks = document.getElementsByClassName("tablinks");
+          for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+          }
+          document.getElementById(cityName).style.display = "block";
+          evt.currentTarget.className += " active";
+        }
+        
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+        </script>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-sm bg-info navbar-dark menuindex flex-top">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">TravelVn</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+
+              <div class="collapse navbar-collapse menuindex-item" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Trang Chủ</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Về Chúng Tôi</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Khám Phá</a>
+                  </li>    
+                </ul>
+                  
+                <ul class="navbar-nav ml-auto ">
+                  <!-- Authentication Links -->
+                  @guest
+                      @if (Route::has('login'))
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                          </li>
+                      @endif
+
+                      @if (Route::has('register'))
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                          </li>
+                      @endif
+                  @else
+                      <li class="nav-item dropdown">
+                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              {{ Auth::user()->name }}
+                          </a>
+
+                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="{{ route('logout') }}"
+                                 onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                  {{ __('Logout') }}
+                              </a>
+
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                  @csrf
+                              </form>
+                          </div>
+                      </li>
+                  @endguest
+              </ul>
+
+              </div>
+            </div>
+          </nav>
+                        
+          
+       
+     		
+    
+       
+      
+
+        <main class="">
+            @yield('content')
+        </main>
+
+        <div>
+            @include('layouts.footer')
+        </div>
+    </div>
+</body>
+</html>
